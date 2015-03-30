71db5145a31b72f14915fd5edd31d8dd6022545b
Marcin Kadłubowski <cinek260@gmail.com>
2015-03-30 08:33:28 +0200

    Fix:add '0' before hours and minutes display when length<2

--- pq.angular-date-filter-polish.js
+++ pq.angular-date-filter-polish.js
@@ -2,11 +2,19 @@
 
 (function() {
     angular.module('pqAngularDateFilterPolish', []).filter('datePolish', function() {
-    	var months = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'];
-    	var date;
+        var months = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'];
+        var date;
         return function(input) {
-        	date = new Date(input);
-        	return date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
+            date = new Date(input);
+            if (date.getHours().length < 2)
+                date.hours = '0' + date.getHours();
+            else
+                date.hours = '0' + date.getHours();
+            if (date.getMinutes().length < 2)
+                date.minutes = '0' + date.getMinutes();
+            else
+                date.minutes = '0' + date.getMinutes();
+            return date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear() + ' ' + date.hours + ':' + date.minutes;
         };
     });
 
