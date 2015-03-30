6030999ce86eeb52dcff72069ce965a538911d78
Marcin Kadłubowski <cinek260@gmail.com>
2015-03-30 12:56:16 +0200

    Fix:SafariSupport

--- pq.angular-date-filter-polish.js
+++ pq.angular-date-filter-polish.js
@@ -5,16 +5,18 @@
         var months = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'];
         var date;
         return function(input) {
-            date = new Date(input);
-            if (date.getHours().length < 2) {
+            input = input.split(/[^0-9]/);
+            date = new Date(input[0], input[1] - 1, input[2], input[3], input[4], input[5]);
+            console.log(date);
+            if (date.getHours() < 10) {
                 date.hours = '0' + date.getHours();
             } else {
-                date.hours = '0' + date.getHours();
+                date.hours = date.getHours();
             }
-            if (date.getMinutes().length < 2) {
+            if (date.getMinutes() < 10) {
                 date.minutes = '0' + date.getMinutes();
             } else {
-                date.minutes = '0' + date.getMinutes();
+                date.minutes = date.getMinutes();
             }
             return date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear() + ' ' + date.hours + ':' + date.minutes;
         };
